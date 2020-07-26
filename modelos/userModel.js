0c5ba15dbdf11cf434bd7ac32254d820c7f94252
paola.estefania <pen.castro@yavirac.edu.ec>
2020-07-25 20:33:00 -0500

    servidor

---
+++ modelos/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userModel = Schema({
+  id: { type: Number },
+  titulo: { type: String },
+  nombre: { type: String },
+  apellido: { type: String },
+  email:  { 
+    type: String,
+    require: true,
+    trim: true, //se le quita  los espacios en blanco
+    unique: true,
+    
+  },
+  passw: { type: String },
+  createAt: { type: String },
+  sessionID: { type: String }
+});
+
+module.exports = mongoose.model("usuarios", userModel);
