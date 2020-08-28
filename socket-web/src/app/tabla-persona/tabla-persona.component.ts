d99281805697c4d74c8a7c9c17196ab47e70e154
Entx <sio.coque@yavirac.edu.ec>
2020-08-28 12:37:18 -0500

    Charts en tablero

--- socket-web/src/app/tabla-persona/tabla-persona.component.ts
+++ socket-web/src/app/tabla-persona/tabla-persona.component.ts
@@ -3,7 +3,8 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { WebServiceService } from '../servicios/web-service.service';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
+import {Chart} from 'chart.js'
 
 
 @Component({
@@ -13,6 +14,8 @@ import { HttpClient } from '@angular/common/http'
 })
 export class TablaPersonaComponent implements  OnInit {
   user=[];
+  chart = [];
+  doughnut =[];
 
   private url:string;
   constructor( private servidor: WebServiceService,
@@ -22,7 +25,54 @@ export class TablaPersonaComponent implements  OnInit {
   
   ngOnInit() {
     this.getPersonas();
-    
+    this.chart =new Chart('canvas',{
+      type:'line',
+      data: {
+        labels: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+        datasets: [
+          {
+            type: 'bar',
+            label: 'My First dataset',
+            data: [243, 156, 365, 30, 156, 265, 356, 543],
+            backgroundColor: 'rgba(255,0,255,0.4)',
+            borderColor: 'rgba(255,0,255,0.4)',
+            fill: false,
+          },
+          {
+            type: 'bar',
+            label: 'My Second dataset',
+            data: [243, 156, 365, 30, 156, 265, 356, 543].reverse(),
+            backgroundColor: 'rgba(0,0,255,0.4)',
+            borderColor: 'rgba(0,0,255,0.4)',
+            fill: false,
+          }
+        ]
+      }
+    });
+
+    this.doughnut =  new Chart('doughnut',{
+      type: 'doughnut',
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Doughnut Chart'
+        },legend: {
+					position: 'top',
+				},animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+      },
+      data: {
+				datasets: [{
+					data: [45,10,5,25,15],
+					backgroundColor: ["red","orange","yellow","green","blue"],
+					label: 'Dataset 1'
+				}],
+				labels: ['Red','Orange','Yellow','Green','Blue']
+			}
+    })
   }
 
   // applyFilter(event: Event) {
