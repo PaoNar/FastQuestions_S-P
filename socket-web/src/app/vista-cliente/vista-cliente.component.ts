933fff49a997cea9f1128818e96c47ffec0fef2d
Entx <sio.coque@yavirac.edu.ec>
2020-09-08 15:58:13 -0500

    algunos cambios

--- socket-web/src/app/vista-cliente/vista-cliente.component.ts
+++ socket-web/src/app/vista-cliente/vista-cliente.component.ts
@@ -19,6 +19,7 @@ import { HttpClient } from '@angular/common/http';
 export class VistaClienteComponent implements OnInit {
   private url:string;
   preguntas: Array<any> = [];
+  respuestasEncuestaForm: FormGroup;
 
   constructor(private fb: FormBuilder, private crudService: CrudService, private servidor: WebServiceService, private http: HttpClient) {
     this.url=servidor.obtenerUrl();
@@ -27,6 +28,10 @@ export class VistaClienteComponent implements OnInit {
 
   ngOnInit(): void {
     this.getDatosEncuesta();
+
+    this.respuestasEncuestaForm = this.fb.group({
+      respuestas: this.fb.array([]),
+    });
   }
 
   getDatosEncuesta(): void {
@@ -38,5 +43,64 @@ export class VistaClienteComponent implements OnInit {
         this.preguntas = data.data[0].contenido
         console.log(this.preguntas)
       });
+  }
+
+  onChange(pregunta: string, opcionSeleccionada: any) {
+    const respuestasEncuestaFormArray = <FormArray>this.respuestasEncuestaForm.controls.respuestas;
+    
+    let respuestas = this.respuestasEncuestaForm.get("respuestas").value,
+      tempOpcion,
+      tempPregunta
+
+    if (respuestas.length == 0) {
+      respuestasEncuestaFormArray.push(new FormControl(
+        {
+          user: localStorage.getItem("encuestaID"),
+          pregunta,
+          opcion: opcionSeleccionada
+        }
+      ));
+      tempPregunta = pregunta;
+      tempOpcion = opcionSeleccionada;
+      console.log(this.respuestasEncuestaForm.get("respuestas").value);
+    }
+
+    respuestas.forEach(element => {
+      if (element.pregunta == pregunta) {
+        respuestasEncuestaFormArray.patchValue([
+          {
+            user: localStorage.getItem("encuestaID"),
+            pregunta: element.pregunta,
+            opcion: opcionSeleccionada
+          }
+        ])
+        console.log(this.respuestasEncuestaForm.get("respuestas").value);
+        console.log("patch");
+      } 
+      // if (element.pregunta != tempPregunta) {
+      //   respuestasEncuestaFormArray.push(new FormControl(
+      //     {
+      //       user: localStorage.getItem("encuestaID"),
+      //       pregunta,
+      //       opcion: opcionSeleccionada
+      //     }
+      //   ));
+      //   counter = 1;
+      //   console.log(counter)
+      //   console.log(this.respuestasEncuestaForm.get("respuestas").value)
+      // }
+    });
+
+    if (tempPregunta != pregunta) {
+      respuestasEncuestaFormArray.push(new FormControl(
+        {
+          user: localStorage.getItem("encuestaID"),
+          pregunta,
+          opcion: opcionSeleccionada
+        }
+      ))
+      tempPregunta = pregunta;
+      console.log(this.respuestasEncuestaForm.get("respuestas").value);
+    }
   }
 }
